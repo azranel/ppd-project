1c463fc92e113e600e1eaba5f76c84a36c08895a
dajvido <dawid.jaskot@gmail.com>
2015-06-09 16:16:44 +0200

    Add more queries for PrologAsker

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,15 @@ import prolog.asker.PrologAsker;
  * Created by azranel on 07.06.15.
  */
 public class Main {
+    private static String PROLOG_PATH = "src/main/prolog/baza.pl";
+
     public static void main(String args[]) {
-        new PrologAsker("src/main/prolog/baza.pl").findByCity("torun");
+        new PrologAsker(PROLOG_PATH).findByCountry("polska");
+        new PrologAsker(PROLOG_PATH).findByCity("torun");
+        new PrologAsker(PROLOG_PATH).findByRegion("miasto");
+        new PrologAsker(PROLOG_PATH).findByAttraction("zamek");
+        new PrologAsker(PROLOG_PATH).findByMaxPrice(1000);
+        new PrologAsker(PROLOG_PATH).findByMaxPriceForPeople(1000, 2);
         new MainFrame();
     }
 
--- src/main/java/prolog/asker/PrologAsker.java
+++ src/main/java/prolog/asker/PrologAsker.java
@@ -1,7 +1,6 @@
 package prolog.asker;
 
 import jpl.Query;
-import jpl.Variable;
 
 import java.util.Hashtable;
 
@@ -9,17 +8,46 @@ import java.util.Hashtable;
  * Created by azranel on 09.06.15.
  */
 public class PrologAsker {
+    private String BRAK = "BRAK";
+
     public PrologAsker(String filePath) {
-        if(!Query.hasSolution("consult('" + filePath + "').")) {
+        if (!Query.hasSolution("consult('" + filePath + "').")) {
             throw new IllegalArgumentException("filePath is not a valid path. Check if it's good.");
         }
     }
 
     //wycieczka(KR, M, R, A, K, D, T, W, Z)
+    public void findByCountry(String countryName) {
+        System.out.println("Looking for country...");
+        Query query = new QueryBuilder("wycieczka")
+                .addNewAtom(countryName)
+                .addNewVariables("M", "R", "A", "K", "D", "T", "W", "Z")
+                .buildQuery();
+
+        System.out.println(query.toString());
+        if (query.hasSolution()) {
+            System.out.println("Wycieczki do " + countryName);
+            Hashtable[] solutions = query.allSolutions();
+            for (Hashtable solution : solutions) {
+                String mValue = solution.get("M").toString();
+                String rValue = solution.get("R").toString();
+                String aValue = solution.get("A").toString();
+                String kValue = solution.get("K").toString();
+                String dValue = solution.get("D").toString();
+                String tValue = solution.get("T").toString();
+                String wValue = solution.get("W").toString();
+                String zValue = solution.get("Z").toString();
+                System.out.println("Wycieczka do " + mValue + ", rejon: " + rValue + ", atrakcje: " + aValue + ", koszt: " + kValue + ", na " + dValue + " dni, transport: " + tValue + ", wyzywienie: " + wValue + ", zakwaterowanie: " + zValue);
+            }
+        } else
+            System.out.println(BRAK);
+    }
+
     public void findByCity(String cityName) {
         System.out.println("Looking for city...");
         Query query = new QueryBuilder("wycieczka")
-                .addNewVariable("KR").addNewAtom(cityName)
+                .addNewVariable("KR")
+                .addNewAtom(cityName)
                 .addNewVariables("R", "A", "K", "D", "T", "W", "Z")
                 .buildQuery();
 
@@ -36,9 +64,118 @@ public class PrologAsker {
                 String tValue = solution.get("T").toString();
                 String wValue = solution.get("W").toString();
                 String zValue = solution.get("Z").toString();
-                System.out.println("Wycieczka do " + krValue + " rejon: " + rValue + ", atrakcje: " + aValue + ", koszt: " + kValue + ", na " + dValue + " dni, transport: " + tValue + ", wyzywienie: " + wValue + ", zakwaterowanie: " + zValue);
+                System.out.println("Wycieczka do " + krValue + ", rejon: " + rValue + ", atrakcje: " + aValue + ", koszt: " + kValue + ", na " + dValue + " dni, transport: " + tValue + ", wyzywienie: " + wValue + ", zakwaterowanie: " + zValue);
             }
         } else
-            System.out.println("BRAK");
+            System.out.println(BRAK);
+    }
+
+    public void findByRegion(String regionName) {
+        System.out.println("Looking for region...");
+        Query query = new QueryBuilder("wycieczka")
+                .addNewVariables("KR", "M")
+                .addNewAtom(regionName)
+                .addNewVariables("A", "K", "D", "T", "W", "Z")
+                .buildQuery();
+
+        System.out.println(query.toString());
+        if (query.hasSolution()) {
+            System.out.println("Wycieczki w region: " + regionName);
+            Hashtable[] solutions = query.allSolutions();
+            for (Hashtable solution : solutions) {
+                String krValue = solution.get("KR").toString();
+                String mValue = solution.get("M").toString();
+                String aValue = solution.get("A").toString();
+                String kValue = solution.get("K").toString();
+                String dValue = solution.get("D").toString();
+                String tValue = solution.get("T").toString();
+                String wValue = solution.get("W").toString();
+                String zValue = solution.get("Z").toString();
+                System.out.println("Wycieczka do " + mValue + "(" + krValue + "), atrakcje: " + aValue + ", koszt: " + kValue + ", na " + dValue + " dni, transport: " + tValue + ", wyzywienie: " + wValue + ", zakwaterowanie: " + zValue);
+            }
+        } else
+            System.out.println(BRAK);
+    }
+
+    public void findByAttraction(String attractionName) {
+        System.out.println("Looking for attraction...");
+        Query query = new QueryBuilder("wycieczka")
+                .addNewVariables("KR", "M", "R")
+                .addNewAtom(attractionName)
+                .addNewVariables("K", "D", "T", "W", "Z")
+                .buildQuery();
+
+        System.out.println(query.toString());
+        if (query.hasSolution()) {
+            System.out.println("Wycieczki zawierajace atrakcje: " + attractionName);
+            Hashtable[] solutions = query.allSolutions();
+            for (Hashtable solution : solutions) {
+                String krValue = solution.get("KR").toString();
+                String mValue = solution.get("M").toString();
+                String rValue = solution.get("R").toString();
+                String kValue = solution.get("K").toString();
+                String dValue = solution.get("D").toString();
+                String tValue = solution.get("T").toString();
+                String wValue = solution.get("W").toString();
+                String zValue = solution.get("Z").toString();
+                System.out.println("Wycieczka do " + mValue + "(" + krValue + "), rejon: " + rValue + ", koszt: " + kValue + ", na " + dValue + " dni, transport: " + tValue + ", wyzywienie: " + wValue + ", zakwaterowanie: " + zValue);
+            }
+        } else
+            System.out.println(BRAK);
+    }
+
+    public void findByMaxPrice(int price) {
+        System.out.println("Looking for attraction...");
+        Query query = new QueryBuilder("cena_max")
+                .addNewInteger(price)
+                .addNewVariables("KR", "M", "R", "A", "K", "D", "T", "W", "Z")
+                .buildQuery();
+
+        System.out.println(query.toString());
+        if (query.hasSolution()) {
+            System.out.println("Wycieczki za max " + price + "zl");
+            Hashtable[] solutions = query.allSolutions();
+            for (Hashtable solution : solutions) {
+                String krValue = solution.get("KR").toString();
+                String mValue = solution.get("M").toString();
+                String rValue = solution.get("R").toString();
+                String aValue = solution.get("A").toString();
+                String kValue = solution.get("K").toString();
+                String dValue = solution.get("D").toString();
+                String tValue = solution.get("T").toString();
+                String wValue = solution.get("W").toString();
+                String zValue = solution.get("Z").toString();
+                System.out.println("Wycieczka do " + mValue + "(" + krValue + "), rejon: " + rValue + ", atrakcje: " + aValue + ", koszt: " + kValue + ", na " + dValue + " dni, transport: " + tValue + ", wyzywienie: " + wValue + ", zakwaterowanie: " + zValue);
+            }
+        } else
+            System.out.println(BRAK);
+    }
+
+    public void findByMaxPriceForPeople(int price, int people) {
+        System.out.println("Looking for attraction...");
+        Query query = new QueryBuilder("cena_max")
+                .addNewInteger(price)
+                .addNewInteger(people)
+                .addNewVariables("KR", "M", "R", "A", "K", "D", "T", "W", "Z")
+                .buildQuery();
+
+        System.out.println(query.toString());
+        if (query.hasSolution()) {
+            System.out.println("Wycieczki za max " + price + "zl");
+            Hashtable[] solutions = query.allSolutions();
+            for (Hashtable solution : solutions) {
+                String krValue = solution.get("KR").toString();
+                String mValue = solution.get("M").toString();
+                String rValue = solution.get("R").toString();
+                String aValue = solution.get("A").toString();
+                String kValue = solution.get("K").toString();
+                String dValue = solution.get("D").toString();
+                String tValue = solution.get("T").toString();
+                String wValue = solution.get("W").toString();
+                String zValue = solution.get("Z").toString();
+                System.out.println("Wycieczka do " + mValue + "(" + krValue + "), rejon: " + rValue + ", atrakcje: " + aValue + ", koszt: " + kValue + ", na " + dValue + " dni, transport: " + tValue + ", wyzywienie: " + wValue + ", zakwaterowanie: " + zValue);
+            }
+        } else
+            System.out.println(BRAK);
     }
 }
