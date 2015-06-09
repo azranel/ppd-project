5b8d74272e2c025435201ff6d517f5bbf3932036
dajvido <dawid.jaskot@gmail.com>
2015-06-09 08:04:41 +0200

    Add logic to prolog class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,47 @@ import java.util.Hashtable;
  */
 public class Main {
     public static void main(String args[]) {
-        if (!Query.hasSolution("consult('/home/azranel/Documents/PPD/dane.pl').")) {
+        if (!Query.hasSolution("consult('src/main/prolog/baza.pl').")) {
             System.out.println("Consult failed");
         } else {
-            test3a("85");
+//            test3a("85");
+            find_by_city("torun");
         }
+    }
+
+    private static void find_by_city(String city) {
+        System.out.println("Looking for city...");
+        Variable KR = new Variable("KR");
+        Variable R = new Variable("R");
+        Variable A = new Variable("A");
+        Variable K = new Variable("K");
+        Variable D = new Variable("D");
+        Variable T = new Variable("T");
+        Variable W = new Variable("W");
+        Variable Z = new Variable("Z");
+        jpl.Atom qcity = new Atom(city);
+
+        //wycieczka(KR, M, R, A, K, D, T, W, Z)
+        Query query = new Query("wycieczka", new Term[]{KR, qcity, R, A, K, D, T, W, Z});
+
+        System.out.println(query.toString());
+        if (query.hasSolution()) {
+            System.out.println("Wycieczki do " + city);
+            Hashtable[] solutions = query.allSolutions();
+            for (Hashtable solution : solutions) {
+                String krValue = solution.get("KR").toString();
+                String rValue = solution.get("R").toString();
+                String aValue = solution.get("A").toString();
+                String kValue = solution.get("K").toString();
+                String dValue = solution.get("D").toString();
+                String tValue = solution.get("T").toString();
+                String wValue = solution.get("W").toString();
+                String zValue = solution.get("Z").toString();
+                System.out.println("Wycieczka do " + krValue + " rejon: " + rValue + ", atrakcje: " + aValue + ", koszt: " + kValue + ", na " + dValue + " dni, transport: " + tValue + ", wyzywienie: " + wValue + ", zakwaterowanie: " + zValue);
+            }
+        } else
+            System.out.println("BRAK");
+
     }
 
     private static void test3a(String sent) {
@@ -22,16 +58,15 @@ public class Main {
         jpl.Integer age = new Integer(Long.valueOf(sent));
 
         //creating query object to make a query to prolog code.
-        Query q3 = new Query("wiek", new Term[] { X, age });
+        Query q3 = new Query("wiek", new Term[]{X, age});
         System.out.println(q3.toString());
-        if(q3.hasSolution()) {
+        if (q3.hasSolution()) {
             Hashtable[] solutions = q3.allSolutions();
-            for(Hashtable solution : solutions) {
+            for (Hashtable solution : solutions) {
                 String xValue = solution.get("X").toString();
                 System.out.println("Ta osoba ma " + sent + " lat: " + xValue);//get the value stored in X
             }
-        }
-        else
+        } else
             System.out.println("NIE MA");
     }
 }
