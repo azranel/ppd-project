e2a7d37cba392124c8b21354907e640b8ab516f5
azranel <chunkyo@gmail.com>
2015-06-11 01:34:25 +0200

    Added new trips to db

--- src/main/prolog/baza.pl
+++ src/main/prolog/baza.pl
@@ -37,6 +37,18 @@ atrakcje(mogilno, ufo).
 wycieczka(polska, torun, miasto, zamek, 700, 5, autokar, sniadanie, hotel).
 wycieczka(polska, swinoujscie, morze, plaza, 1400, 7, pociag, sniadanie, osrodek).
 wycieczka(polska, gniezno, miasto, brama, 300, 1, samochod, brak, brak).
+wycieczka(polska, zakopane, miasto, gory, 1000, 7, pociag, all_inclusive, hotel).
+wycieczka(polska, zakopane, miasto, gory, 1100, 7, samochod, all_inclusive, hotel).
+wycieczka(polska, zakopane, miasto, gory, 1200, 10, pociag, all_inclusive, hotel).
+wycieczka(polska, zakopane, miasto, gory, 1400, 10, samochod, all_inclusive, hotel).
+wycieczka(tunezja, nabul, miasto, morze, 1700, 11, samolot, sniadanie, hotel).
+wycieczka(tunezja, nabul, miasto, morze, 1300, 7, samolot, sniadanie, hotel).
+wycieczka(tunezja, nabul, miasto, morze, 2000, 11, samolot, all_inclusive, hotel).
+wycieczka(tunezja, kabis, miasto, morze, 1400, 7, samolot, all_inclusive, hotel).
+wycieczka(niemcy, berlin, miasto, zabytki, 2400, 10, samochod, all_inclusive, hotel).
+wycieczka(niemcy, berlin, miasto, zabytki, 150, 1, pociag, brak, brak).
+wycieczka(niemcy, berlin, miasto, zabytki, 3200, 14, samolot, all_inclusive, hotel).
+wycieczka(niemcy, monachium, miasto, zabytki, 2400, 10, samochod, all_inclusive, hotel).
 wycieczka(francja, paryz, miasto, wieza, 1700, 2, samolot, all_inclusive, hotel).
 
 krajowa(M, R, A, K, D, T, W, Z) :- wycieczka(polska, M, R, A, K, D, T, W, Z).
